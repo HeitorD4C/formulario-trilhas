6f3822cc2551fa46773f44782fb1b4775bb3c08d
HeitorD4C <heitoralenk@gmail.com>
2025-03-27 16:49:06 -0300

    Responsividade da Index (1/2)

--- js/loguin.js
+++ js/loguin.js
@@ -1,33 +1,55 @@
 const inputs = document.querySelectorAll('.campo__input');
+const buttons = document.querySelectorAll('.botao');
 
-inputs.forEach(function (input) {
-    input.setAttribute(required, '');
-    input.addEventListener('blur', function(e) {
-        inputValidate(input, e);
-    })
+buttons.forEach(function (button) {
+    button.addEventListener('click', () => {
+        if(button.classList.contains('botao--secundario')) {
+            window.location.href = 'register.html';
+        }else{
+            if(inputValidate() === true) {
+                window.location.href = 'index.html';
+            }
+        }
+    });
 });
 
-function inputValidate(input, event) {
-    if(!input.validity.valid){
-        addSpanTag(input);
-    }
+function inputValidate() {
+    isValid = null;
+    
+    inputs.forEach(function(input) {
+        if(!input.validity.valid || input.value === ''){
+            addSpan(input);
+            isValid = false;
+        }else {
+            removeSpan(input);
+            isValid = true;
+        }
+    });
+
+    return isValid
 }
 
-function addSpanTag(input) {
+function addSpan(input) {
     const parent = input.closest('.campo');
     const existingSpan = parent.querySelector('.span--aviso');
-    
+
     if(existingSpan) {
         return
     }
 
+    const loguinOrPassword = (input.getAttribute('type') === 'password') ? 'Senha incorreta' : 'Usuário incorreto';
     const span = document.createElement('span');
+
+    span.textContent = `${loguinOrPassword}`;
     span.classList.add('span--aviso');
-
-    const typeInput = input.getAttribute('type');
-    const loguinOrPassword = (typeInput === 'password') ? 'Senha' : 'Usuário';
-
-    span.textContent = `${loguinOrPassword} incorreto!`;
-
     parent.appendChild(span);
+}
+
+function removeSpan(input) {
+    const parent = input.closest('.campo');
+    const existingSpan = parent.querySelector('.span--aviso');
+
+    if(existingSpan) {
+        parent.removeChild(existingSpan);
+    }
 }
